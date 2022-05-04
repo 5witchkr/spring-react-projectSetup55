cb9b7d4ea0411e7abcae3353f3e514ffb6883941
5witchkr <hob4419@gmail.com>
2022-05-04 17:33:51 +0900

    mongodb-connect

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,16 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.ContentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class DemoController {
+    private final ContentService contentService;
 
-    @GetMapping("demo")
-    public List<String> demo() {
-        return Arrays.asList("hello","world");
+    @GetMapping("/dbcall")
+    public String demo() {
+        contentService.mongoInsert();
+        return "test";
     }
 }
